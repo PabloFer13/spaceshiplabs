7b4e6ea73ff07715de92c8a806419a268cb61671
erick <erick@spaceshiplabs.com>
2020-03-18 17:05:30 -0500

    add links to 'menciones'

--- src/components/menciones/index.jsx
+++ src/components/menciones/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, GrisSquare } from './index.styled';
+import { Grid, GridSquare, GridSquareNoLink } from './index.styled';
 
 import imco from '../../images/imco.png';
 import cnn from '../../images/cnn.png';
@@ -13,13 +13,25 @@ import opendata from '../../images/opendata.png';
 import sipse from '../../images/sipse.png';
 
 const menciones = [
-  imco, cnn, telemundo, televisa, eluniversal,
-  excelsior, lajornada, animalpolitico, opendata, sipse,
+  { img: imco, url: 'https://imco.org.mx/capital_humano/conferencia-de-prensa-mejora-tu-escuela/' },
+  { img: cnn, url: 'https://www.youtube.com/watch?v=lJWqpp09oDY&feature=youtu.be' },
+  { img: telemundo, url: false },
+  { img: televisa, url: 'https://noticieros.televisa.com/foro-tv-fractal/1409/imco-mejora-tu-escuela/' },
+  { img: eluniversal, url: 'https://www.eluniversal.com.mx/blogs/colectivo-mexico-informate/2015/07/8/el-gobierno-como-plataforma' },
+  { img: excelsior, url: false },
+  { img: lajornada, url: false },
+  { img: animalpolitico, url: 'https://www.animalpolitico.com/click-necesario/pide-tus-legisladores-cambiar-la-leytelecom/' },
+  { img: opendata, url: 'https://www.opendata500.com/mx/list/' },
+  { img: sipse, url: 'https://sipse.com/novedades/lanzan-aplicacion-de-mapa-interactivo-de-gasolineras-en-el-pais-98763.html' },
 ];
 
 const MencionesComponent = () => (
   <Grid flexWrap>
-    {menciones.map((mencion) => <GrisSquare image={mencion} />)}
+    {menciones.map((mencion) => (
+      mencion.url
+        ? <GridSquare href={mencion.url} target="_blank" image={mencion.img} />
+        : <GridSquareNoLink image={mencion.img} />
+    ))}
   </Grid>
 );
 
--- src/components/menciones/index.styled.js
+++ src/components/menciones/index.styled.js
@@ -1,7 +1,39 @@
 import styled from 'styled-components';
 import { Rows } from '../../theme';
 
-export const GrisSquare = styled.div`
+export const GridSquare = styled.a`
+  flex-basis: 50%;
+  box-sizing: border-box;
+  border: 1px solid transparent;
+  height: 120px;
+  background: transparent url(${({ image }) => (image)}) no-repeat center center;
+  ${({ theme }) => (theme.MediaQueryMediumSmall)}{
+    flex-basis: 33.333%;
+    height: 200px;
+    border-bottom-color: ${({ theme }) => (theme.Gray)};
+    border-right-color: ${({ theme }) => (theme.Gray)};
+    &:nth-child(n+10){
+      border-bottom-color: transparent;
+    }
+    &:nth-child(3n){
+      border-right-color: transparent;
+    }
+  }
+  ${({ theme }) => (theme.MediaQueryMedium)}{
+    flex-basis: 20%;
+    &:nth-child(n+6){
+      border-bottom-color: transparent;
+    }
+    &:nth-child(3n){
+      border-right-color: ${({ theme }) => (theme.Gray)};
+    }
+    &:nth-child(5n){
+      border-right-color: transparent;
+    }
+  }
+`;
+
+export const GridSquareNoLink = styled.div`
   flex-basis: 50%;
   box-sizing: border-box;
   border: 1px solid transparent;
